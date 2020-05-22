27e5557a34fe7c8952eb34ad5d3ad2f03b7235d2
Che-Wei (Joanne) Chou <37817830+kgweicat@users.noreply.github.com>
2020-05-22 08:39:07 -0500

    Update IDedObject.java

--- IDedObject.java
+++ IDedObject.java
@@ -1,14 +1,8 @@
 package hashdictionary;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
  *
- * @author Che-Wei Chou
+ * @author Che-Wei (Joanne) Chou
  */
 //an interface
 public interface IDedObject {
